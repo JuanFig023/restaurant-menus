f65b621848c37a719409589d1d86308e38a303d8
JuanFig023 <juanseb023@gmail.com>
2025-03-05 23:12:55 -0600

    Set up Sequelize, created models, created tests for model instances

--- index.test.js
+++ index.test.js
@@ -18,26 +18,48 @@ describe('Restaurant and Menu Models', () => {
 
     test('can create a Restaurant', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const restaurant = await Restaurant.create(seedRestaurant[0]);
+        expect(restaurant.name).toEqual(seedRestaurant[0].name);
+        expect(restaurant.location).toEqual(seedRestaurant[0].location)
+        expect(restaurant.cuisine).toEqual(seedRestaurant[0].cuisine)
     });
 
     test('can create a Menu', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const menu = await Menu.create(seedMenu[0]);
+        expect(menu.title).toEqual(seedMenu[0].title);
     });
 
     test('can find Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const restaurant = await Restaurant.create(seedRestaurant[0]);
+        await restaurant.update({name: "Flanigans", location: "Florida", cuisine: "American"});
+        expect(restaurant.name).toEqual("Flanigans");
+        expect(restaurant.location).toEqual("Florida");
+        expect(restaurant.cuisine).toEqual("American");
     });
 
     test('can find Menus', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const menu = await Menu.create(seedMenu[0]);
+        await menu.update({title: "Brunch"});
+        expect(menu.title).toEqual("Brunch");
     });
 
     test('can delete Restaurants', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const restaurant = await Restaurant.create(seedRestaurant[0]);
+        await restaurant.destroy();
+        const foundRestaurant = await Restaurant.findByPk(restaurant.id);
+        expect(foundRestaurant).toBeNull();
     });
+
+    test('can delete Menu', async () => {
+        // TODO - write test
+        const menu = await Menu.create(seedMenu[0]);
+        await menu.destroy();
+        const foundMenu = await Menu.findByPk(Menu.id);
+        expect(foundMenu).toBeNull();
+    });
+
 })
--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,18 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
 
 // TODO - create a Menu model
+
+class Menu extends Model{};
+
+Menu.init({
+        title: DataTypes.STRING
+    },
+    {
+        sequelize,
+        modelName: 'Menu'
+    }
+)
+
 
 module.exports = {Menu};
